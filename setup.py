964d1bbe3302ec1ab7880ef29797c87c5c4ba0e8
Chenqqian Zhang <100290172+Chengqian-Zhang@users.noreply.github.com>
2023-01-06 21:41:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dflow-phonon",
+    version="0.0.1",
+    author="Chengqian Zhang",
+    author_email="2043899742@qq.com",
+    description="A phonon calculation package based on dflow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chengqian-Zhang/dflow-phonon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    provides=["dflow-phonon"],
+    scripts=["scripts/dflow-phonon"]
+)
