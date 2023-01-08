a3437c349e28f39a29cba9ec591107c5f05b272d
Chengqian Zhang <2000011006@stu.pku.edu.cn>
2023-01-08 21:18:49 +0800

    version 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dflow-phonon",
-    version="0.0.15",
+    version="0.0.16",
     author="Chengqian Zhang",
     author_email="2043899742@qq.com",
     description="A phonon calculation package based on dflow",
